20a3e5c0dc46a257878d7ab3f93cb68770e70ab0
PercyGFX <isurangabtk@gmail.com>
2024-05-12 23:51:27 +0530

    sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import {
-  Package2,
-  Home,
-  ShoppingCart,
-  Package,
   Plus,
   EllipsisVertical,
+  SquareArrowUp,
+  Radio,
+  HardDrive,
+  Calendar,
+  Building2,
+  Bell,
+  Book,
+  ArrowLeftFromLine,
 } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 
 export default function Page() {
@@ -19,33 +24,67 @@ export default function Page() {
         <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
           <Link
             href="#"
-            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            //className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            className="flex h-9 w-9 items-center justify-center rounded-lg text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
-            <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-            <span className="sr-only">Acme Inc</span>
+            <SquareArrowUp className="h-6 w-6 transition-all group-hover:scale-110" />
+            <span className="sr-only">Trending Up</span>
           </Link>
 
           <Link
             href="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            className="bg-[#2962FF] flex h-9 w-9 items-center justify-center rounded-lg text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
-            <Home className="h-5 w-5" />
-            <span className="sr-only">Dashboard</span>
+            <Radio className="h-6 w-6" />
+            <span className="sr-only">Radio</span>
           </Link>
 
           <Link
             href="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            className="flex h-9 w-9 items-center justify-center rounded-lg  text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
-            <ShoppingCart className="h-5 w-5" />
+            <HardDrive className="h-6 w-6" />
             <span className="sr-only">Orders</span>
           </Link>
 
           <Link
             href="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            className="flex h-9 w-9 items-center justify-center rounded-lg  text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
-            <Package className="h-5 w-5" />
+            <Calendar className="h-6 w-6" />
+            <span className="sr-only">Products</span>
+          </Link>
+
+          <Link
+            href="#"
+            className="flex h-9 w-9 items-center justify-center rounded-lg  text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+          >
+            <Building2 className="h-6 w-6" />
+            <span className="sr-only">Products</span>
+          </Link>
+
+          <Link
+            href="#"
+            className="flex h-9 w-9 items-center justify-center rounded-lg  text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+          >
+            <Bell className="h-6 w-6" />
+            <span className="sr-only">Products</span>
+          </Link>
+
+          <Link
+            href="#"
+            className="flex h-9 w-9 items-center justify-center rounded-lg  text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+          >
+            <Book className="h-6 w-6" />
+            <span className="sr-only">Products</span>
+          </Link>
+          <Separator />
+
+          <Link
+            href="#"
+            className=" bg-secondary flex h-9 w-9 items-center justify-center rounded-lg text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+          >
+            <ArrowLeftFromLine className="h-5 w-5 text-background" />
             <span className="sr-only">Products</span>
           </Link>
         </nav>
@@ -69,10 +108,7 @@ export default function Page() {
           </div>
           {/* header end */}
 
-         <div className=" h-2/4 border-b">
-          {/* chart area */}
-         
-          </div>
+          <div className=" h-2/4 border-b">{/* chart area */}</div>
 
           <div>data table</div>
         </div>
